d5febe71baf3b6615af8222c41cc07ce3661e988
MarkBaggett <lo127001@gmail.com>
2021-06-11 10:24:34 -0700

    example of pxpowershell with powershell scripts

---
+++ pxpowershell/example_dir2iso.py
@@ -0,0 +1,130 @@
+import pxpowershell
+import argparse
+
+
+powershell_script = r"""function New-IsoFile 
+{  
+  <#  
+   .Synopsis  
+    Creates a new .iso file  
+   .Description  
+    The New-IsoFile cmdlet creates a new .iso file containing content from chosen folders  
+   .Example  
+    New-IsoFile "c:\tools","c:Downloads\utils"  
+    This command creates a .iso file in $env:temp folder (default location) that contains c:\tools and c:\downloads\utils folders. The folders themselves are included at the root of the .iso image.  
+   .Example 
+    New-IsoFile -FromClipboard -Verbose 
+    Before running this command, select and copy (Ctrl-C) files/folders in Explorer first.  
+   .Example  
+    dir c:\WinPE | New-IsoFile -Path c:\temp\WinPE.iso -BootFile "${env:ProgramFiles(x86)}\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg\efisys.bin" -Media DVDPLUSR -Title "WinPE" 
+    This command creates a bootable .iso file containing the content from c:\WinPE folder, but the folder itself isn't included. Boot file etfsboot.com can be found in Windows ADK. Refer to IMAPI_MEDIA_PHYSICAL_TYPE enumeration for possible media types: http://msdn.microsoft.com/en-us/library/windows/desktop/aa366217(v=vs.85).aspx  
+   .Notes 
+    NAME:  New-IsoFile  
+    AUTHOR: Chris Wu 
+    Modified: Mark Baggett 6-10-21 - Do not include base folder. 
+ #>
+ 
+  [CmdletBinding(DefaultParameterSetName='Source')]Param( 
+    [parameter(Position=1,Mandatory=$true,ValueFromPipeline=$true, ParameterSetName='Source')]$Source,  
+    [parameter(Position=2)][string]$Path = "$env:temp\$((Get-Date).ToString('yyyyMMdd-HHmmss.ffff')).iso",  
+    [ValidateScript({Test-Path -LiteralPath $_ -PathType Leaf})][string]$BootFile = $null, 
+    [ValidateSet('CDR','CDRW','DVDRAM','DVDPLUSR','DVDPLUSRW','DVDPLUSR_DUALLAYER','DVDDASHR','DVDDASHRW','DVDDASHR_DUALLAYER','DISK','DVDPLUSRW_DUALLAYER','BDR','BDRE')][string] $Media = 'DVDPLUSRW_DUALLAYER', 
+    [string]$Title = (Get-Date).ToString("yyyyMMdd-HHmmss.ffff"),  
+    [switch]$Force, 
+    [switch]$SkipGetFirst,
+    [parameter(ParameterSetName='Clipboard')][switch]$FromClipboard 
+  ) 
+  
+  Begin {  
+    ($cp = new-object System.CodeDom.Compiler.CompilerParameters).CompilerOptions = '/unsafe' 
+    if (!('ISOFile' -as [type])) {  
+      Add-Type -CompilerParameters $cp -TypeDefinition @'
+public class ISOFile  
+{ 
+  public unsafe static void Create(string Path, object Stream, int BlockSize, int TotalBlocks)  
+  {  
+    int bytes = 0;  
+    byte[] buf = new byte[BlockSize];  
+    var ptr = (System.IntPtr)(&bytes);  
+    var o = System.IO.File.OpenWrite(Path);  
+    var i = Stream as System.Runtime.InteropServices.ComTypes.IStream;  
+   
+    if (o != null) { 
+      while (TotalBlocks-- > 0) {  
+        i.Read(buf, BlockSize, ptr); o.Write(buf, 0, bytes);  
+      }  
+      o.Flush(); o.Close();  
+    } 
+  } 
+}  
+'@  
+    } 
+   
+    if ($BootFile) { 
+      if('BDR','BDRE' -contains $Media) { Write-Warning "Bootable image doesn't seem to work with media type $Media" } 
+      ($Stream = New-Object -ComObject ADODB.Stream -Property @{Type=1}).Open()  # adFileTypeBinary 
+      $Stream.LoadFromFile((Get-Item -LiteralPath $BootFile).Fullname) 
+      ($Boot = New-Object -ComObject IMAPI2FS.BootOptions).AssignBootImage($Stream) 
+    } 
+  
+    $MediaType = @('UNKNOWN','CDROM','CDR','CDRW','DVDROM','DVDRAM','DVDPLUSR','DVDPLUSRW','DVDPLUSR_DUALLAYER','DVDDASHR','DVDDASHRW','DVDDASHR_DUALLAYER','DISK','DVDPLUSRW_DUALLAYER','HDDVDROM','HDDVDR','HDDVDRAM','BDROM','BDR','BDRE') 
+  
+    Write-Verbose -Message "Selected media type is $Media with value $($MediaType.IndexOf($Media))"
+    ($Image = New-Object -com IMAPI2FS.MsftFileSystemImage -Property @{VolumeName=$Title}).ChooseImageDefaultsForMediaType($MediaType.IndexOf($Media)) 
+   
+    if (!($Target = New-Item -Path $Path -ItemType File -Force:$Force -ErrorAction SilentlyContinue)) { Write-Error -Message "Cannot create file $Path. Use -Force parameter to overwrite if the target file already exists."; break } 
+  }  
+  
+  Process { 
+    if($FromClipboard) { 
+      if($PSVersionTable.PSVersion.Major -lt 5) { Write-Error -Message 'The -FromClipboard parameter is only supported on PowerShell v5 or higher'; break } 
+      $Source = Get-Clipboard -Format FileDropList 
+    } 
+  
+    foreach($item in gci $Source) { 
+      if($item -isnot [System.IO.FileInfo] -and $item -isnot [System.IO.DirectoryInfo]) { 
+        $item = Get-Item -LiteralPath $item
+      } 
+  
+      if($item) { 
+        Write-Verbose -Message "Adding item to the target image: $($item.FullName)"
+        try { $Image.Root.AddTree($item.FullName, $true) } catch { Write-Error -Message ($_.Exception.Message.Trim() + ' Try a different media type.') } 
+      } 
+    } 
+  } 
+  
+  End {  
+    if ($Boot) { $Image.BootImageOptions=$Boot }  
+    $Result = $Image.CreateResultImage()  
+    [ISOFile]::Create($Target.FullName,$Result.ImageStream,$Result.BlockSize,$Result.TotalBlocks) 
+    Write-Verbose -Message "Target image ($($Target.FullName)) has been created"
+    $Target
+  } 
+}
+
+"""
+#Script such as this will break pxpowershell without at least one new lines after the }.  Without it the prompt never returns
+
+
+def dir2iso(src, dest, title="Created by pxpowershell"):
+    pshell = pxpowershell.PxPowershell(debug=False)
+    x = pshell.start_process()
+    x = pshell.run(powershell_script, 10)
+    x = pshell.run(f"cd {src}")
+    result = pshell.run(f"New-IsoFile {src} -path {dest} -title {title} -Force", 60*60)
+    result = pshell.run(f"ls {dest}")
+    pshell.stop_process()
+    return result
+
+def main():
+    #parse cli and make ISO
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source", help = "The path to the directory to turn into an ISO")
+    parser.add_argument("destination", help="The destination ISO file to create (including path).")
+    parser.add_argument("title",help="The title of the ISO.")
+    args = parser.parse_args()
+    print(dir2iso(args.source, args.destination, args.title))
+
+
+if __name__ == "__main__":
+    main()
